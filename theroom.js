f24007c79de762d65d19e776a0a0a5bc718f7734
Tyler Parker <btylerparker@gmail.com>
2013-08-13 19:43:01 -0400

    You are tearing me apart Lisa\!

---
+++ theroom.js
@@ -0,0 +1,25 @@
+function randomTheRoom() {
+  
+    this.theRoomOriginal = $(this).html();
+    
+    $(this).click(function() {
+      $(this).html(this.theRoomOriginal);
+    });
+    
+    // add derped class
+    $(this).addClass("roomed");
+
+    var randomIndex = Math.floor((Math.random()*quoteArray.length));
+    return '<p>'+quoteArray[randomIndex]+ '</p>';
+
+}
+
+
+$('.comment-text, p.ctx').not('.roomed').html(randomTheRoom);
+
+
+setInterval(function() {
+
+  $('.comment-text, p.ctx').not('.roomed').html(randomTheRoom);
+}, 100);
+
